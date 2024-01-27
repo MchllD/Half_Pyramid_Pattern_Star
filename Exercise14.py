aa63eb4a9c101022922610f037ab9562f0064920
MchllD <mich11dimafelix@gmail.com>
2024-01-27 22:42:14 +0800

    outer loop

---
+++ Exercise14.py
@@ -0,0 +1,17 @@
+# Exercise 14: Print a downward Half-Pyramid Pattern of Star (asterisk)
+
+# pseudocode
+
+# function to print a downward half pyramid
+# create outer loop for rows (starting from the given number of rows and decreasing)
+# Inner loop for printing '*' based on the current row value
+# Move to the next line after printing '*' for the current row
+# Set the number of rows for the downward half pyramid
+
+# ------------------------------- pseudocode ---------------------------------------------------
+
+
+# function to print a downward half pyramid
+def print_downward_half_pyramid(rows):
+    # create outer loop for rows (starting from the given number of rows and decreasing)
+    for i in range(rows, 0, -1):
